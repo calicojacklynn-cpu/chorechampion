ec497a34aa22f9104a4fc62c173804d93761a623
Calico Sloan <calicojacklynn@gmail.com>
2026-02-11 11:56:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8504294104-77dc9",
+  "appId": "1:275254204162:web:996bb3a71a32fedb33541b",
+  "apiKey": "AIzaSyBUdf4LhNJ1ob-DRQs9K5KUoFCScAZw0ds",
+  "authDomain": "studio-8504294104-77dc9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "275254204162"
+};
