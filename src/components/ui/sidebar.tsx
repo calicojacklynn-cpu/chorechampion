293e676be04213c45428d268fa1381426e00eea6
Calico Sloan <calicojacklynn@gmail.com>
2026-02-12 12:20:37 +0000

    still transparent on the other themes.

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -203,7 +203,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] p-0 text-sidebar-foreground bg-sidebar [&>button]:hidden"
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
@@ -214,7 +214,7 @@ const Sidebar = React.forwardRef<
             <SheetHeader className="sr-only">
               <SheetTitle>Menu</SheetTitle>
             </SheetHeader>
-            <div className="flex h-full w-full flex-col bg-sidebar">{children}</div>
+            <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
         </Sheet>
       )
