c44b339c39cedd07f035bfec533a94fdb4e11d02
Calico Sloan <calicojacklynn@gmail.com>
2026-02-12 12:15:25 +0000

    on mobile, the side bar is completely transparent.

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -203,7 +203,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] p-0 text-sidebar-foreground [&>button]:hidden"
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
@@ -214,7 +214,7 @@ const Sidebar = React.forwardRef<
             <SheetHeader className="sr-only">
               <SheetTitle>Menu</SheetTitle>
             </SheetHeader>
-            <div className="flex h-full w-full flex-col">{children}</div>
+            <div className="flex h-full w-full flex-col bg-sidebar">{children}</div>
           </SheetContent>
         </Sheet>
       )
