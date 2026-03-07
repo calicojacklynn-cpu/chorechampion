8cc9955e44d7085a33eb3e2a5e3b6d2700d25d0a
Calico Sloan <calicojacklynn@gmail.com>
2026-03-07 22:50:22 +0000

    fix: remove all imports to satisfy webpack

--- src/app/api/genkit/[...flow]/route.ts
+++ src/app/api/genkit/[...flow]/route.ts
@@ -1,11 +1,2 @@
-// import { genkitHandler } from '@genkit-ai/next';
-// 
-// // This imports and registers all the flows and initializes Genkit.
-// import '@/ai';
-// 
-// // This exports the handler that will process requests for the flows.
-// export const { GET, POST } = genkitHandler();
-
-export const GET = () => new Response('Genkit under maintenance');
+export const GET = () => new Response('Under Maintenance');
 export const POST = GET;
-// Refreshing build cache
